f6c134f7b980333eeea4400886415d556a6ad09f
Camilo <camilo.inuneko@gmail.com>
2021-06-15 10:34:50 -0400

    se actualizaron las funciones de lista_flujos
    se actualizo las funcinones para un mejor manejo de las tablas de ingreso de informacion

--- mineralism/src/components/Lista_flujos.js
+++ mineralism/src/components/Lista_flujos.js
@@ -11,19 +11,37 @@ const Lista_flujos=({type='Alimentacion'})=>{
 const Titulo=type;
 const [list,setList]=useState(['R1']);
 const [vector,setVector]=useState('prueba');
-
+/*
 
 const addCelda=()=>{
 
-    setList((list)=>[...list,'R1'])
-}
+    setList((list)=>[...list,'R2'])
+    console.log(type+'Nombre')
+}*/
 const inputRef = useRef(null)
 const openCelda=(list)=>{
 
 
 setVector(list)
-context[1]()
-console.log(context[1])
+console.log(list+type+'Nombre')
+if(sessionStorage.getItem(list+type+'Nombre')!=null){
+const nombre=   sessionStorage.getItem(list+type+'Nombre')
+const densidad=  sessionStorage.getItem(list+type+'Densidad')
+const solido=    sessionStorage.getItem(list+type+'Solido')
+const ley=      sessionStorage.getItem(list+type+'Ley')
+const caudal=    sessionStorage.getItem(list+type+'Caudal')
+document.getElementById('inputNombre'+type).value=nombre
+document.getElementById('inputdDensidad'+type).value=densidad
+document.getElementById('inputSolido'+type).value=solido
+document.getElementById('inputLey'+type).value=ley
+document.getElementById('inputCaudal'+type).value=caudal}
+else{
+    document.getElementById('inputNombre'+type).value=''
+document.getElementById('inputdDensidad'+type).value=''
+document.getElementById('inputSolido'+type).value=''
+document.getElementById('inputLey'+type).value=''
+document.getElementById('inputCaudal'+type).value=''
+}
 }
 
     return <>    
@@ -32,19 +50,20 @@ console.log(context[1])
     <div className={'col-10'}>
         <h4 >{Titulo}</h4></div>
         <div className={'col-2'}>
-        <button onClick={addCelda}>+</button>            
+        <button data-bs-toggle={"modal"} 
+data-bs-target={"#Modal"+type}  onClick={()=>{/*addCelda()*/;openCelda()}}>+</button>            
         </div>
         </div>
 <div className={'row'}><div className={'col border'}>
     <ul>
 { list.map(
 (list)=>{ return<li><button type={"button"} className={"btn btn-primary"} data-bs-toggle={"modal"} 
-data-bs-target={"#Modal"+vector+type} onClick={()=>openCelda(list)}>{list}</button></li>}
+data-bs-target={"#Modal"+type} onClick={()=>openCelda(list)}>{list}</button></li>}
     )}
     </ul>
     
     </div></div>
-<Input_vector  id={vector} type={Titulo}/>
+<Input_vector f={[list,setList]} id={vector} type={Titulo}/>
 
     </div>
     </>
