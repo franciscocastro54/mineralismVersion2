75eeb2d027ae206e1fb2f0a4014283e0dd4f433c
Camilo <camilo.inuneko@gmail.com>
2021-06-15 02:34:04 -0400

    Arreglo error de comit

--- mineralism/src/components/Barra_lateral.js
+++ mineralism/src/components/Barra_lateral.js
@@ -12,28 +12,28 @@ const Barra=()=>{
 
 
 
-return <div class="accordion Barra" id="accordionExample">
-<div class="accordion-item">
-  <h2 class="accordion-header" id="headingOne">
-    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+return <div className="accordion Barra" id="accordionExample">
+<div className="accordion-item">
+  <h2 className="accordion-header" id="headingOne">
+    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
       <strong>Diagramas</strong>
     </button>
   </h2>
-  <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-    <div class="accordion-body">
+  <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+    <div className="accordion-body">
     <a onClick={()=>{console.log('hola mundo')}}><p>Balance Polpaico</p></a>
     <a onClick={()=>{console.log('hola mundo')}}><p>Circuito Completo</p></a>
     </div>
   </div>
 </div>
-<div class="accordion-item">
-  <h2 class="accordion-header" id="headingTwo">
-    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+<div className="accordion-item">
+  <h2 className="accordion-header" id="headingTwo">
+    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
    <strong>Reportes</strong>
     </button>
   </h2>
-  <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-    <div class="accordion-body">
+  <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+    <div className="accordion-body">
       {/*Aqui van los reportes */}
     </div>
   </div>
