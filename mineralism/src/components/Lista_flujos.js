ac4209cf67209138c2859a6aa292b9686aaabcee
Camilo <camilo.inuneko@gmail.com>
2021-07-17 00:21:05 -0400

    actualizacion 0:20 17/07/2021

--- mineralism/src/components/Lista_flujos.js
+++ mineralism/src/components/Lista_flujos.js
@@ -70,11 +70,14 @@ sessionStorage.setItem('ListaBalance'+type,JSON.stringify(list))
                     <h4 >{Titulo}</h4></div>
                 <div className={'col-2'}>
                     <button data-bs-toggle={"modal"}
-                        data-bs-target={"#Modal" + type} onClick={() => {/*addCelda()*/; openCelda() }}>+</button>
+                    className={'btn '}    data-bs-target={"#Modal" + type} onClick={() => {/*addCelda()*/; openCelda() }}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+                  </svg></button>
                 </div>
             </div>
-            <div className={'row'}><div className={'col border '} >
-                <ul className={'list-group'}>
+            <div className={'row'}>
+                <div className={'col border px-0'} style={{minHeight: '15em',maxHeight:'15em'}}>
+                <ul className={'list-group scrollspy-example'}>
                     {list.map(
                         (list) => {
                             return <li><button type={"button"} className={"btn btn-primary"} data-bs-toggle={"modal"}
